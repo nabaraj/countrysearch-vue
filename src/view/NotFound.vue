fa48494bbf5ee835c2b558a56024decd82eaf233
Nabaraj Saha <nabara2k@gmail.com>
2021-08-29 18:08:20 +0530

    redesign not found page

--- src/view/NotFound.vue
+++ src/view/NotFound.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>Not Found</div>
+  <div>
+    <Header />
+    <b-container>
+      <router-link id="backLink" to="/"
+        ><b-button class="mb-4 borderButtonLink shadow-sm themeFormControll"
+          ><b-icon-arrow-left /> Back</b-button
+        ></router-link
+      >
+      <div>Not Found</div>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import Header from "./../components/Header";
 export default {
   name: "NotFound",
+  components: {
+    Header,
+  },
 };
 </script>
 
