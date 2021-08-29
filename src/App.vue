4c621b4168ce22f9369e114452509bfd4b6a1720
Nabaraj Saha <nabara2k@gmail.com>
2021-08-29 15:56:07 +0530

    handling service worker update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    <router-view />
+    <b-overlay :show="updateExists" rounded="sm">
+      <router-view />
+      <template v-slot:overlay>
+        <div class="text-center">
+          <b-icon icon="cloud-download" font-scale="3"></b-icon>
+          <p id="cancel-label">New updates are available!</p>
+          <b-button
+            variant="outline-darkBlue"
+            size="sm"
+            aria-describedby="cancel-label"
+            @click="refreshApp"
+          >
+            Refresh
+          </b-button>
+        </div>
+      </template>
+    </b-overlay>
   </div>
 </template>
 
@@ -8,6 +24,38 @@
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      refreshing: false,
+      registration: null,
+      updateExists: false,
+    };
+  },
+  created() {
+    // Listen for swUpdated event and display refresh snackbar as required.
+    document.addEventListener("swUpdated", this.showRefreshUI, { once: true });
+    // Refresh all open app tabs when a new service worker is installed.
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      if (this.refreshing) return;
+      this.refreshing = true;
+      window.location.reload();
+    });
+  },
+  methods: {
+    showRefreshUI(e) {
+      this.registration = e.detail;
+      this.updateExists = true;
+    },
+    refreshApp() {
+      // Handle a user tap on the update app button.
+      this.updateExists = false;
+      // Protect against missing registration.waiting.
+      if (!this.registration || !this.registration.waiting) {
+        return;
+      }
+      this.registration.waiting.postMessage("skipWaiting");
+    },
+  },
 };
 </script>
 
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -7,11 +7,14 @@ if (process.env.NODE_ENV === "production") {
     ready() {
       console.log(
         "App is being served from cache by a service worker.\n" +
-          "For more details, visit https://goo.gl/AFskqB"
+        "For more details, visit https://goo.gl/AFskqB"
       );
     },
-    registered() {
-      console.log("Service worker has been registered.");
+    registered(registration) {
+      console.log('Service worker has been registered.');
+      setInterval(() => {
+        registration.update();
+      }, 1000 * 60 * 60); // hourly checks
     },
     cached() {
       console.log("Content has been cached for offline use.");
@@ -19,8 +22,11 @@ if (process.env.NODE_ENV === "production") {
     updatefound() {
       console.log("New content is downloading.");
     },
-    updated() {
+    updated(registration) {
       console.log("New content is available; please refresh.");
+      document.dispatchEvent(
+        new CustomEvent('swUpdated', { detail: registration })
+      );
     },
     offline() {
       console.log(
